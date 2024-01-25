d6b3fcf536a59ec5375087d264f238f2999555ad
Nx0dle <ramenkeebs@proton.me>
2024-01-25 20:32:20 +0100

    show groups, subcategories and tasks in UI

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,9 @@
 class MainController < ApplicationController
   skip_before_action :authenticate_user!, only: [:welcome]
   def index
+    @groups = Group.all
+    @lists = List.all
+    @subcategories = Subcategory.all
     @tasks = Task.all
   end
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,17 +5,6 @@ import "jquery"
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    let l_panel = document.querySelector('#left-panel')
-    let m_panel = document.querySelector('#middle-panel')
-    let r_panel = document.querySelector('#right-panel')
-
-    let collapse = document.querySelector('span.collapse')
-
-    collapse.addEventListener('click', () => {
-        r_panel.style.display = 'none'
-        document.body.style.gridTemplate = '1fr / 1fr 4fr'
-    })
-
     let task_completed = document.querySelectorAll('details .item input')
 
     task_completed.forEach( (e) => {
@@ -37,22 +26,6 @@ window.addEventListener('DOMContentLoaded', () => {
         if (!e.checked) {
             e.parentElement.classList.remove('done')
             e.parentElement.classList.remove('const-done')
-        }
-    })
-
-    let grid = document.querySelector('span.grid')
-    let iterView = 0
-
-    grid.addEventListener('click', () => {
-        iterView++
-        if (iterView % 2 === 0) {
-            let mid_panel_grid = document.querySelector('#middle-panel .lists')
-            mid_panel_grid.style.display = 'grid'
-            mid_panel_grid.style.gridTemplate = '1fr 1fr 1fr / 1fr 1fr 1fr'
-        }
-        else {
-            let mid_panel_grid = document.querySelector('#middle-panel .lists')
-            mid_panel_grid.style.display = 'flex'
         }
     })
 })
